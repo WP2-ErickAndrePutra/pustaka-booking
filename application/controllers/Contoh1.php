de743082264b7045dfdc6b533fcdb1ca46c96012
WP2-ErickAndrePutra <erickandreputra12@gmail.com>
2024-03-26 20:09:35 +0700

    First Commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<br>Nama saya Erick Andre Putra saya tinggal di daerah Depok hobby saya adalah bermain musik.";
+    }
+}
